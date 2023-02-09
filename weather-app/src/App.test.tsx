585e8cf5a36a482eac2063b8b75ec519efa90724
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-09 15:23:00 -0400

    feat: added test for the FormBox component

--- weather-app/src/App.test.tsx
+++ weather-app/src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import FormBox from './components/FormBox/FormBox.component';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('should render the app component', () => {
+    render(<App />);
+
+    const appContainer = screen.getByTestId('app-container');
+
+    expect(appContainer).toBeInTheDocument();
+  });
+
+  it('should render the FormBox', () => {
+    render(<App />);
+
+    const formBoxComponent = screen.getByTestId('form-box');
+
+    expect(formBoxComponent).toBeInTheDocument();
+  });
 });
+
--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,7 +1,10 @@
+import FormBox from "./components/FormBox/FormBox.component";
+
 function App(): JSX.Element {
-  return <>
+  return <div data-testid="app-container">
     <h1>Previsão do Tempo</h1>
-  </>
+    <FormBox />
+  </div>
 }
 
 export default App;
